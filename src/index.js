acd085c53ef69aa206d6b1dbd10f52d7297ca027
leogaleanoq <leogaleanoq@gmail.com>
2021-09-03 20:20:25 +0200

    introduccion jsx

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import ImagenIronman from './assets/images/ironman.png';
 //ejemplo con el React DOM
-
+// se crea la constante con la info del usuario
+const usuario = {
+  Nombre: 'Leo',
+  Apellido: 'Galeano',
+  Correo: 'leo.galeano@email.com',
+};
+//se crea la funcion para obtener la información de la constante usuario
+function getInfo(usuario){
+  if(usuario){
+    return `${usuario.Nombre} ${usuario.Apellido}`;
+  }else{
+    return `Hola extraño!`
+  };
+};
 //se crea un elemento con el dom usanto jsx
-const elementDom = <h1>Hola en ReactDom!</h1>
+const elementDom = <div>
+    <img src={ImagenIronman} />
+    <h1>Bienvenido {getInfo(usuario)}</h1>
+  </div>
 //se crea un nuevo contenedor con el id del html
-const container2 = document.getElementById('root');
+const container = document.getElementById('root');
 //se renderiza el elemento en el contenedor creado
-ReactDOM.render(elementDom, container2);
+ReactDOM.render(elementDom, container);
