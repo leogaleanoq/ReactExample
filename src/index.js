28db4688824257e918a23824a681f5bb6965c132
leogaleanoq <leogaleanoq@gmail.com>
2021-09-05 16:03:36 +0200

    primer componente

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ImagenIronman from './assets/images/ironman.png';
-//ejemplo con el React DOM
-// se crea la constante con la info del usuario
-const usuario = {
-  Nombre: 'Leo',
-  Apellido: 'Galeano',
-  Correo: 'leo.galeano@email.com',
-};
-//se crea la funcion para obtener la información de la constante usuario
-function getInfo(usuario){
-  if(usuario){
-    return `${usuario.Nombre} ${usuario.Apellido}`;
-  }else{
-    return `Hola extraño!`
-  };
-};
-//se crea un elemento con el dom usanto jsx
-const elementDom = <div>
-    <img src={ImagenIronman} />
-    <h1>Bienvenido {getInfo(usuario)}</h1>
-  </div>
+import Card from './components/Card.js';
+
 //se crea un nuevo contenedor con el id del html
 const container = document.getElementById('root');
+
 //se renderiza el elemento en el contenedor creado
-ReactDOM.render(elementDom, container);
+ReactDOM.render(<Card/>, container);
