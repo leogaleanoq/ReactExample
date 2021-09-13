1fb5383c2e77040a5878d5fd832317ee4fa09285
leogaleanoq <leogaleanoq@gmail.com>
2021-09-13 19:44:02 +0200

    props

--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 const container = document.getElementById('root');
 
 //se renderiza el elemento en el contenedor creado
-ReactDOM.render(<Card/>, container);
+ReactDOM.render(<Card 
+                    title="Technique Guide"
+                    description="Learn amazing street workout and calisthenics"
+                    img="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png"
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+                />, container);
