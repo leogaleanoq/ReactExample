ee19ba72bde431f9dbd1769dd3eb8a05fc98bf7e
leogaleanoq <leogaleanoq@gmail.com>
2021-09-23 16:37:17 +0200

    introduccion a estados

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,23 @@ import circlesimg from '../assets/images/circles.png';
 
 class Card extends React.Component{
 
+    constructor(props){
+        super(props);
+        //Creamos el estado inicial
+        this.state = {
+            image: exerciseimg
+        };
+    };
+    //funcion que se invoca despues de que se genera un componente
+    componentDidMount(){
+        //se establece un intervalo de 5 segundos para cambiar el estado de la imagen
+        setTimeout(()=>{
+            this.setState({
+                image: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
+            })
+        }, 5000)
+    };
+
     render(){
         const { title, description, img, leftColor, rightColor} = this.props
         return (
@@ -15,7 +32,8 @@ class Card extends React.Component{
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={img} className="float-right" alt=""/>
+                            {/* el source de la imagen se obtiene del estado incial del constructor */}
+                            <img src={this.state.image} className="float-right" alt=""/>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
