c1416aa131d51eb452fea3a6357c5c9d1f2a14a7
leogaleanoq <leogaleanoq@gmail.com>
2021-09-24 18:15:16 +0200

    simplificacion de componentes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import exerciseimg from '../assets/images/exercise.png';
 import '../assets/styles/card.css';
 import circlesimg from '../assets/images/circles.png';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Card extends React.Component{
-
-    constructor(props){
-        super(props);
-        //Creamos el estado inicial
-        this.state = {
-            image: exerciseimg
-        };
-    };
-    //funcion que se invoca despues de que se genera un componente
-    componentDidMount(){
-        //se establece un intervalo de 5 segundos para cambiar el estado de la imagen
-        setTimeout(()=>{
-            this.setState({
-                image: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
-            })
-        }, 5000)
-    };
-
     render(){
         const { title, description, img, leftColor, rightColor} = this.props
         return (
-            <div className="card mx-auto Fitness-Card"
+            <div className="card mx-auto Fitness-Card mt-2"
             style={{
                 backgroundImage:`url(${circlesimg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
             }}>
@@ -33,7 +15,7 @@ class Card extends React.Component{
                     <div className="row center">
                         <div className="col-6">
                             {/* el source de la imagen se obtiene del estado incial del constructor */}
-                            <img src={this.state.image} className="float-right" alt=""/>
+                            <img src={img} className="float-right" alt=""/>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import Wellcome from '../components/welcome';
-import Card from '../components/card';
+import Wellcome from '../components/welcome.js';
+import ExerciseList from '../components/exerciseList.js';
+import Add from '../components/add.js'
 
 class Exercises extends React.Component{
+    constructor(props){
+        super(props);
+        //en los estados, se crea el objeto array data con 3 objetos
+        this.state = {
+            //es la data con la información de las cards
+            data: [{
+                    "id": 1,
+                    "title": "Technique Guides",
+                    "description": "Learn amazing street workout and calisthenics",
+                    "img": "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06",
+                    "leftColor": "#A74CF2",
+                    "rightColor": "#617BFB"
+                },{
+                    "id": 2,
+                    "title": "Skills Training",
+                    "description": "Learn the secrets of bodyweight techniques",
+                    "img": "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercises02.png?alt=media&token=a5d55381-5f3e-4f25-92dd-560775f96aa2",
+                    "leftColor": "#17EAD9",
+                    "rightColor": "#6078EA"
+                },{
+                    "id": 3,
+                    "title": "Strength Training",
+                    "description": "Train anytime, everywere and become a superhero!",
+                    "img": "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise03.png?alt=media&token=8e5301c0-151e-415d-bd2c-655235d9c916",
+                    "leftColor": "#FAD961",
+                    "rightColor": "#F76B1C"
+                }
+            ]
+        }
+    }
     render(){
         return(
             <div>
                 <Wellcome
                     username="Leo"
                 />
-                <Card 
-                    title="Technique Guide"
-                    description="Learn amazing street workout and calisthenics"
-                    img="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png"
-                    rightColor="#617BFB"
-                    leftColor="#A74CF2"
+                <ExerciseList
+                    //las props del componente llamado exerciseList se igualan al array dentro del estado llamado data
+                    excercisesprops = {this.state.data}
                 />
+                <Add/>
             </div>
         )
     }
