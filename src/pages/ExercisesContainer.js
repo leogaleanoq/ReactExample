b419643d35ea773cbb2493c5ba905876f64a6c95
leogaleanoq <leogaleanoq@gmail.com>
2021-10-15 13:05:13 +0200

    hooks en exercises

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,49 +1,45 @@
-import React from 'react';
+//los hooks se identificando con la palabra use
+import React,{useState, useEffect} from 'react';
 import Loading from '../components/loading.js';
 import Error500 from '../pages/Error500.js';
 import Exercises from './Exercises.js';
 
-//usamos property initializers, es un feature de babel que permite no inicializar las props en el constructor
+const ExercisesConstainer = () =>{
 
-class ExercisesConstainer extends React.Component{
-    //en los estados, se crea el objeto array data con 3 objetos
-    state = {
-        //es la data con la información de las cards
-        data: [],
-        loading: true,
-        error: null
-    };
+    //variables de estado
+    const [data, setData] = useState([]); 
+    const [loading, setLoading] = useState(true); 
+    const [error, setError] = useState(null); 
 
-    async componentDidMount(){
-        await this.fetchExercises();
-    };
-
-    fetchExercises = async() =>{
-        try{
-
-            let res = await fetch('http://localhost:8000/api/exercises');
-            let data = await res.json();
-            this.setState({data, loading:false});
-            console.log(data);
-
-        }catch(error){
-            this.setState({
-                loading:false,
-                error
-            });
+    useEffect(()=>{
+        const fetchExercises = async() =>{
+            try{
+    
+                let res = await fetch('http://localhost:8000/api/exercises');
+                let data = await res.json();
+                setData(data);
+                setLoading(false);
+    
+            }catch(error){
+                setLoading(false);
+                setError(error);
+            }
         }
-    }
-    render(){
-        if(this.state.loading){
-            return <Loading/>;
-        };
-        if(this.state.error){
-            return <Error500/>;
-        };
+        fetchExercises();
+    },[]);
+
+    if(loading){
+        return <Loading/>;
+    }else if(error){
+        return <Error500/>;
+    }else{
         return<Exercises
-            data={this.state.data}
+            data={data}
         />
-    }
+    };
+
 };
+
+
 
 export default ExercisesConstainer;
