b5f38b97b9aa9f62c4a8f195d0c0192ac3bc2d6e
leogaleanoq <leogaleanoq@gmail.com>
2021-10-15 13:11:35 +0200

    generalizando hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = url =>{
+
+    //variables de estado
+    const [data, setData] = useState([]); 
+    const [loading, setLoading] = useState(true); 
+    const [error, setError] = useState(null); 
+
+
+    useEffect(()=>{
+        const fetchResourse = async() =>{
+            try{
+    
+                let res = await fetch(url);
+                let data = await res.json();
+                setData(data);
+                setLoading(false);
+    
+            }catch(error){
+                setLoading(false);
+                setError(error);
+            }
+        }
+        fetchResourse();
+    },[url]);
+    return{data, loading, error};
+};
+
+export default useFetch;
--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,33 +1,14 @@
 //los hooks se identificando con la palabra use
-import React,{useState, useEffect} from 'react';
+import React from 'react';
 import Loading from '../components/loading.js';
 import Error500 from '../pages/Error500.js';
 import Exercises from './Exercises.js';
+import useFetch from '../hooks/useFetch.js';
 
 const ExercisesConstainer = () =>{
-
-    //variables de estado
-    const [data, setData] = useState([]); 
-    const [loading, setLoading] = useState(true); 
-    const [error, setError] = useState(null); 
-
-    useEffect(()=>{
-        const fetchExercises = async() =>{
-            try{
+    //empleando el hooks importado
+    const {data, loading, error} = useFetch('http://localhost:8000/api/exercises');
     
-                let res = await fetch('http://localhost:8000/api/exercises');
-                let data = await res.json();
-                setData(data);
-                setLoading(false);
-    
-            }catch(error){
-                setLoading(false);
-                setError(error);
-            }
-        }
-        fetchExercises();
-    },[]);
-
     if(loading){
         return <Loading/>;
     }else if(error){
